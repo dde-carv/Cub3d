967e8c8e61b014166794faa36eca6357f57f4963
Duarte Eusébio <dde-carv@student.42lisboa.com>
2025-05-15 14:19:41 +0100

    On the works.

--- sources/cub_utils/error.c
+++ sources/cub_utils/error.c
@@ -6,14 +6,21 @@ int	cub_perror(t_error_type err, t_game *g, char *str, int flag)
 		return (0);
 	clean_cub(g);
 	write(2, "cub3d: ", 7 * (err != end));
-	write(2, "invalid number of arguments!\n", 29);
-	write(2, "error opening file: ", 20);
-	write(2, "invalid file extension!\n", 24);
-	write(2, "file is empty!\n", 15);
-	write(2, "invalid color!\n", 15);
-	write(2, "map is not not closed by walls!\n", 32);
-	write(2, "invalid map!\n", 13);
-	write(2, "invalid character!\n", 19);
-	write(2, "invalid player!\n", 16);
-	write(2, "invalid texture!\n", 17);
+	write(2, "invalid number of arguments!\n", 29 * (err == inv_ac));
+	write(2, "invalid file extension!\n", 24 * (err == inv_ext));
+	write(2, "error opening file: ", 20 * (err == inv_file));
+	write(2, "file is empty!\n", 15 * (err == empty_file));
+	write(2, "invalid color!\n", 15 * (err == inv_color));
+	write(2, "map is not not closed by walls!\n", 32 * (err == inv_wall));
+	write(2, "invalid map!\n", 13 * (err == inv_map));
+	write(2, "invalid character!\n", 19 * (err == inv_char));
+	write(2, "invalid player!\n", 16 * (err == inv_ply));
+	write(2, "invalid texture!\n", 17 * (err == inv_tex));
+	ft_putendl_fd(str, 2);
+	if (((err == inv_ac) || (err == inv_ext)) && ft_putchar_fd("\n", 2))
+		cub_usage(1);
+	if (err == end)
+		exit(0);
+	exit(1);
+	return(1);
 }
